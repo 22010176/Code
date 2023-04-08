893d81b1032960050207f51cdc769f2e78609837
____ <22010176@st.phenikaa-uni.edu.vn>
2023-04-08 13:55:42 +0700

    ed

--- Data & AI/LOGIC.py
+++ Data & AI/LOGIC.py
@@ -20,7 +20,6 @@ def linear_line2(prediction_list, target_list):
     #     print("Error: Prediction and target lists must have the same length.\n")
     #     continue  # Ask user for input again
 
-    # Calculate average X and Y
     n = len(prediction_list)
     average_x = sum(prediction_list) / n
     average_y = sum(target_list) / n
@@ -38,10 +37,8 @@ def linear_line2(prediction_list, target_list):
     variance_y = sum([(y - average_y) ** 2 for y in target_list]) / n
 
     # Calculate MAE, MSE, RMSE, SSR, SST, R-squared, Alter R-squared
-    mae = sum([abs(p - t)
-              for p, t in zip(prediction_list, target_list)]) / n
-    mse = sum(
-        [(p - t) ** 2 for p, t in zip(prediction_list, target_list)]) / n
+    mae = sum([abs(p - t) for p, t in zip(prediction_list, target_list)]) / n
+    mse = sum([(p - t) ** 2 for p, t in zip(prediction_list, target_list)]) / n
     rmse = math.sqrt(mse)
     SSR = sum([(a * x + b - y) ** 2 for x,
                y in zip(prediction_list, target_list)])
@@ -64,30 +61,30 @@ def linear_line2(prediction_list, target_list):
     print(f"Alternative R-squared: {R2:.3f}")
     return average_x, average_y, SSx, SSxy, a, b, mae, mse, R1, R2, SSR, SST
     # Sort the prediction and target lists in ascending order
-    # prediction_list, target_list = zip(
-    #     *sorted(zip(prediction_list, target_list)))
+    prediction_list, target_list = zip(
+        *sorted(zip(prediction_list, target_list)))
 
     # Draw Graph
-    # plt.scatter(prediction_list, target_list)
-    # plt.plot(prediction_list, [
-    #     a * x + b for x in prediction_list], color='red')
+    plt.scatter(prediction_list, target_list)
+    plt.plot(prediction_list, [
+        a * x + b for x in prediction_list], color='red')
 
-    # # Present regression line as dashed lines
-    # for i in range(len(prediction_list)-1):
-    #     x1, y1 = prediction_list[i], target_list[i]
-    #     x2, y2 = prediction_list[i+1], target_list[i+1]
-    #     slope = 1 if i % 2 == 0 else -1
-    #     plt.plot([x1, x2], [y1, y2], color='blue', linestyle='--', linewidth=0.5, alpha=0.5,
-    #              solid_capstyle='round', dash_capstyle='round', dash_joinstyle='round', dashes=[3, 3], zorder=0)
+    # Present regression line as dashed lines
+    for i in range(len(prediction_list)-1):
+        x1, y1 = prediction_list[i], target_list[i]
+        x2, y2 = prediction_list[i+1], target_list[i+1]
+        slope = 1 if i % 2 == 0 else -1
+        plt.plot([x1, x2], [y1, y2], color='blue', linestyle='--', linewidth=0.5, alpha=0.5,
+                 solid_capstyle='round', dash_capstyle='round', dash_joinstyle='round', dashes=[3, 3], zorder=0)
 
-    # for i in range(len(prediction_list)):
-    #     plt.annotate(
-    #         f"({prediction_list[i]}, {target_list[i]})", (prediction_list[i], target_list[i]))
+    for i in range(len(prediction_list)):
+        plt.annotate(
+            f"({prediction_list[i]}, {target_list[i]})", (prediction_list[i], target_list[i]))
 
-    # plt.xlabel('Predicted Values')
-    # plt.ylabel('Target Values')
-    # plt.title('Regression Line Graph')
-    # plt.show()
+    plt.xlabel('Predicted Values')
+    plt.ylabel('Target Values')
+    plt.title('Regression Line Graph')
+    plt.show()
 
     # # Ask user if they want to continue
     # answer = input("\nDo you want to continue? (y/n) \n")
--- Data & AI/testing.py
+++ Data & AI/testing.py
@@ -3,13 +3,43 @@ from LOGIC import linear_line2 as ee
 # from old import linear_line as eee
 from random import *
 import os
+import matplotlib.pyplot as plt
 
-cate = ['aveX', 'aveY', 'SSx', 'SSxy', 'B1',
-        'B0', 'MAE', 'MSE', "R1", 'R2', 'SSR', 'SST']
+
+def drawGrap(*t):
+    # Sort the prediction and target lists in ascending order
+    for x, y, a, b in t:
+        x, y = zip(
+            *sorted(zip(x, y)))
+
+        # Draw Graph
+        plt.scatter(x, y)
+        plt.plot(x, [
+            a * x + b for x in x], color='blue')
+
+        # Present regression line as dashed lines
+        for i in range(len(x)-1):
+            x1, y1 = x[i], y[i]
+            x2, y2 = x[i+1], y[i+1]
+            plt.plot([x1, x2], [y1, y2], color='blue', linestyle='--', linewidth=0.2, alpha=0.1,
+                     solid_capstyle='round', dash_capstyle='round', dash_joinstyle='round', dashes=[3, 3], zorder=0)
+
+        for i in range(len(x)):
+            plt.annotate(
+                f"({x[i]}, {y[i]})", (x[i], y[i]))
+
+    plt.xlabel('Predicted Values')
+    plt.ylabel('Target Values')
+    plt.title('Regression Line Graph')
+    plt.show()
 
 
 def out(value):
     print(f'{value[1]}.{value[2]}')
+
+
+CATE = ['aveX', 'aveY', 'SSx', 'SSxy', 'B1',
+        'B0', 'MAE', 'MSE', "R1", 'R2', 'SSR', 'SST']
 
 
 def test(funcs, *test):
@@ -18,15 +48,19 @@ def test(funcs, *test):
     a.sort(key=lambda x: x[0])
     os.system("cls")
     for i in a:
-        i[2] = {key: val for (key, val) in zip(cate, i[2])}
-    [(out(i), print("_"*100)
-      if (a.index(i)+1) % len(funcs) == 0 else 0) for i in a]
+        i[2] = {key: val for (key, val) in zip(CATE, i[2])}
+        out(i)
+        if (a.index(i)+1) % len(funcs) == 0:
+            print("_"*100)
 
 
 test(
     [my, ee],
 
-    [[i for i in range(0, 200)], [randrange(10, 30) for _ in range(1000)]],
-    [[17, 21, 35, 39, 50, 65], [132, 150, 160, 162, 149, 170]],
-    [[1, 2, 3, 45], [2, 3, 4, 5]]
+    [[i for i in range(0, 200)], [randrange(10, 30) for _ in range(200)]],
+    [[i for i in range(0, 2400)], [randrange(10, 30) for _ in range(2400)]],
+    [[i for i in range(0, 2400)], [randrange(10, 30) for _ in range(2400)]],
+    [[i for i in range(0, 2400)], [randrange(10, 30) for _ in range(2400)]],
+    # [[17, 21, 35, 39, 50, 65], [132, 150, 160, 162, 149, 170]],
+    # [[1, 2, 3, 5], [2, 3, 4, 5]]
 )
