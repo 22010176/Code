bf0be8339ae113c1dbf7b12c3ecb846fadc8bc5a
____ <22010176@st.phenikaa-uni.edu.vn>
2023-04-13 07:28:05 +0700

    edh

--- Data & AI/e.py
+++ Data & AI/e.py
@@ -27,3 +27,27 @@ def B(x, y):
     xy = SSxy(x, y, x0, y0)
     xx = R_(x, x0)
     return B0(y0, xy/xx, x0)
+
+
+def A(x, y):
+    [x0, y0] = TBXY(x, y)
+    return SSxy(x, y, x0, y0)/R_(x, x0)
+
+
+def MakeLine(x, y):
+    return [A(x, y), B(x, y)]
+
+
+def TimX(x, y, y0):
+    [a, b] = MakeLine(x, y)
+    return (y0 - b)/a
+
+
+def TimY(x, y, x0):
+    return create_line(*MakeLine(x, y))(x0)
+
+
+def R2(x, y):
+    line = create_line(*MakeLine(x, y))
+    Y = [line(i) for i in x]
+    return R1(y, Y, aver(y))
