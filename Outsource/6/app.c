c0876ba5169f73a8262d60122d9a9b5e77b07d35
____ <22010176@st.phenikaa-uni.edu.vn>
2023-04-27 22:25:27 +0700

    ed

--- Outsource/6/app.c
+++ Outsource/6/app.c
@@ -1,7 +1,20 @@
 #include "../utils/hea.h"
 #include "../utils/matrices.c"
-#include "../utils/arr.c"
 
 int main() {
 
+  int* size1 = malloc(2 * sizeof(int));
+  int* size2 = malloc(2 * sizeof(int));
+
+  size1[0] = 2;
+  size1[1] = size2[0] = 2;
+  size2[1] = 2;
+
+  float** A = createMatrix(size1);
+  float** B = createMatrix(size2);
+  int* size = malloc(2 * sizeof(int));
+  size[0] = size1[0];
+  size[1] = size2[1];
+
+  printMaxtrics(MatrixMultiply(A, B, size1, size2), size);
 }
--- Outsource/utils/matrices.c
+++ Outsource/utils/matrices.c
@@ -4,7 +4,6 @@ void printMaxtrics(float** a, int* size) {
   float* max = malloc(size[0] * sizeof(float));
   for (int i = 1; i < size[0]; i++) max[i] = findMax(a[i], size[0]);
   int indent = (int)abs(ceil(log10f(findMax(max, size[0]) * 100 + 0.1))) + 2;
-  // printf("%d\n", indent);
   for (int i = 0; i < size[0]; i++) {
     for (int j = 0; j < size[1]; j++) printf("|%*.2f", indent, a[i][j]);
     printf("\n");
@@ -14,10 +13,21 @@ float** createMatrix(int* size) {
   float** A = malloc(size[0] * sizeof(float*));
   for (int i = 0; i < size[0]; i++)
     A[i] = malloc(size[1] * sizeof(float));
-  for (int i = 0; i < size[0]; i++) {
-    for (int j = 0; j < size[1];j++) {
-      A[i][j] = (i * 10 + j) * 100 + 0.02;
-    }
-  }
+  for (int i = 0; i < size[0]; i++)
+    for (int j = 0; j < size[1];j++)
+      A[i][j] = (i * 10 + j);
   return A;
+}
+float** MatrixMultiply(float** A, float** B, int* size1, int* size2) {
+  int* size = autoGenerateArr(2);
+  size[0] = size1[0];
+  size[1] = size2[1];
+  float** R = createMatrix(size);
+  if (size1[1] != size2[0]) return NULL;
+  for (int i = 0; i < size[0];i++)
+    for (int j = 0; j < size[1];j++) {
+      R[i][j] = 0;
+      for (int k = 0; k < size1[1];k++) R[i][j] += A[i][k] * B[k][j];
+    }
+  return R;
 }
