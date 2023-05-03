94e9aee86e23ee13a858426568be9634996ca602
____ <22010176@st.phenikaa-uni.edu.vn>
2023-05-04 05:39:20 +0700

    ed

--- Outsource/11.2/app.c
+++ Outsource/11.2/app.c
@@ -16,17 +16,20 @@ float Test3(int i);
 float Test4(int i);
 float Ox(int i, int j);
 float Oy(int i, int j);
-
+float e(int i) {
+  float z = 0.1 * (i - 2) + 2;
+  return z * z;
+}
 int main() {
   ref = Vector2d(100, 100);
 
-  float** scene1 = FillMatrixFunc1(createMatrix(ref), ref, Test);
-  float** scene3 = FillMatrix(createMatrix(ref), ref, Ox);
-  float** scene5 = FillMatrix(createMatrix(ref), ref, Oy);
-  float** scene2 = FillMatrixFunc1(createMatrix(ref), ref, Test3);
-  float** scene4 = FillMatrixFunc2(createMatrix(ref), ref, Test4);
+  float** scene1 = FillMatrixFunc1(createMatrix(ref), ref, e);
+  float** Ox_ = FillMatrix(createMatrix(ref), ref, Ox);
+  float** Oy_ = FillMatrix(createMatrix(ref), ref, Oy);
+  // float** scene2 = FillMatrixFunc1(createMatrix(ref), ref, Test3);
+  // float** scene4 = FillMatrixFunc2(createMatrix(ref), ref, Test4);
 
-  Render(PreProcess(ref, 5, scene1, scene2, scene3, scene4, scene5), ref);
+  Render(PreProcess(ref, 3, Ox_, Oy_, scene1), ref);
 }
 float Ox(int i, int j) {
   return i == ref[0] - 2;
--- Test/test2.c
+++ Test/test2.c
@@ -1,15 +1,36 @@
 #include <stdio.h>
-#include <stdlib.h>
-struct  testing { int x; };
-struct test2 { struct testing* x; };
-
-
-
-int main() {
-  struct test2 a;
-  a.x = malloc(9 * sizeof(struct test2));
-  for (int i = 0; i < 9; i++) {
-    a.x[i].x = i;
-    printf("%d ", a.x[i].x);
+int Songuyento(int x) {
+  int i;
+  int dem = 0;
+  if (x < 2) {
+    printf("\n%d la: -1", x);
   }
+  else {
+    for (i = 1;i <= x;i++) {
+      if (x % i == 0) {
+        dem += 1;
+      }
+    }
+    if (dem == 2) {
+      printf("\n%d la: 1", x);
+    }
+    else {
+      printf("\n%d la: -1", x);
+    }
+
+  }
+}
+int main() {
+  int Dayso[50];
+  int j, n;
+  printf("Nhap so phan tu cho n:");
+  scanf("%d", &n);
+  for (j = 0;j <= n;j++) {
+    printf("\nDay so[%d] la :", j);
+    scanf("%d", &Dayso[j]);
+  }
+  for (j = 0;j <= n;j++) {
+    Songuyento(Dayso[j]);
+  }
+
 }
