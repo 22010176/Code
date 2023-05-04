657efe3287240cfb9023fdea95def639352f4cb0
a@gmail.com <a@gmail.com>
2023-05-04 08:55:47 +0700

    ed

---
+++ Outsource/vector/vector.c
@@ -0,0 +1,35 @@
+/*
+Create Vector
+Free Vector
+Add Item
+Delete Item
+
+*/
+const size_t VECTOR_INIT_CAPACITY = 4;
+
+typedef struct {
+    void ** data;
+    size_t count;
+    size_t capacity;
+} Vector
+
+
+
+Vector *CreateVector() {
+    Vector *vec = malloc(sizeof(Vector));
+    if (!vec) return NULL;
+
+    vec->count = 0;
+    vec->capacity = VECTOR_INIT_CAPACITY;
+    vec->data = malloc(vector->capacity *sizeof(void*));
+    if (!vector->data) return NULL;
+    return v;
+}
+void FreeVector(Vector *vec) {
+    if (!(vec && vec->data)) return;
+    free(vec->data);
+    free(vec);
+}
+static int _ResizeVector(Vector *vec,size_t capacity) {
+    
+}
