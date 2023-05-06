829c0e58d9e9545543b656f58a24a55f7fc3bf1d
ed <ed@gmai.com>
2023-05-06 11:20:02 +0700

    ed

---
+++ Outsource/E/app.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+string Reverse(string sourse) {
+  string result = "";
+  for (int i = sourse.length() - 1; i >= 0;i--) result += sourse[i];
+  return result;
+}
+string Replace(string source, char reset) {
+
+}
+int main() {
+}
