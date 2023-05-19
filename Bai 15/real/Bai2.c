60d832245519762282073efcc0123273074ed48b
____ <22010176@st.phenikaa-uni.edu.vn>
2023-05-19 19:28:18 +0700

    ed

--- Bai 15/real/Bai2.c
+++ Bai 15/real/Bai2.c
@@ -7,94 +7,73 @@
 char* StripSpace(char*);
 char* format(char*);
 char* StrShift(char*, int);
-
-void nhapN(int* n) {
-  printf("\nNhap so Nguyen: ");
-  scanf("%d", n);
-  fflush(stdin);
+void nhapN(int*);
+void swap(char*, char*);
+char** SortingString(char**, int, int (*)(char*, char*));
+int com(char*, char*);
+int main() {
+  int i, n;
+  // 1
+  char** sinhVien = malloc(50 * sizeof(char*));
+  for (i = 0; i < 50; i++) {
+    sinhVien[i] = calloc(30 + 1, sizeof(char));
+    sinhVien[i][30] = '\0';
+  }
+  // 2
+  nhapN(&n);
+  for (i = 0; i < n;i++) {
+    printf("\nNhap ten sinh vien %d: ", i + 1);
+    gets(sinhVien[i]);
+    fflush(stdin);
+    strcpy(sinhVien[i], format(StripSpace(sinhVien[i])));
+  }
+  // 3
+  SortingString(sinhVien, n, com);
+  for (i = 0; i < n;i++) printf("%d: %s\n", i + 1, sinhVien[i]);
 }
-void swap(char* A, char* B) {
-  char A_[strlen(A)];
-  strcpy(A_, A);  strcpy(A, B);  strcpy(B, A_);
-}
-int _A(char* A, char* B) { return 1; }
-char** SortingString(char** A, int len, int (*compare)(char*, char*)) {
-  if (compare == NULL) compare = _A;
-  for (int i = 0; i < len;i++)
-    for (int j = 0; j < len - i;j++) if (compare(A[j], A[j + 1]) == 1) swap(A[j], A[j + 1]);
-}
-
 int com(char* A, char* B) {
   int lenA = 0, lenB = 0;
   char* a_ = malloc((strlen(A) + 1) * sizeof(char)), * b_ = malloc((strlen(B) + 1) * sizeof(char));
   strcpy(a_, A); strcpy(b_, B);
-  do {
-    char* A_ = strrchr(a_, ' ');
-    char* B_ = strrchr(b_, ' ');
-    A_ = A_ ? A_ : a_;
-    B_ = B_ ? B_ : b_;
+  while (1) {
+    char* A_ = strrchr(a_, ' '), * B_ = strrchr(b_, ' ');
+    A_ = A_ ? A_ : a_, B_ = B_ ? B_ : b_;
     if (!(strlen(a_) && strlen(b_))) return 0;
     if (strcmp(A_, B_)) return strcmp(A_, B_);
-    a_[strlen(a_) - strlen(A_)] = '\0';
-    b_[strlen(b_) - strlen(B_)] = '\0';
-  } while (1);
-}
-int main() {
-  // 1
-  char** sinhVien = malloc(50 * sizeof(char*));
-  for (int i = 0; i < 50; i++) {
-    sinhVien[i] = calloc(30 + 1, sizeof(char));
-    sinhVien[i][30] = '\0';
+    a_[strlen(a_) - strlen(A_)] = b_[strlen(b_) - strlen(B_)] = '\0';
   }
-
-  int i, n = 8;
-  // char a[] = "ASDFASFasfd";
-  strcpy(sinhVien[0], "Do Duc Nam");
-  strcpy(sinhVien[1], "Do Duc Nam");
-  strcpy(sinhVien[6], "Do Duc Nam");
-  strcpy(sinhVien[2], "Dinh Thi Ngoc Anh");
-  strcpy(sinhVien[3], "Do Duc Minh");
-  strcpy(sinhVien[4], "Do Duc Anh");
-  strcpy(sinhVien[7], "Do Duc A");
-  strcpy(sinhVien[5], "Do Duc Minh");
-  // 2
-  // nhapN(&n);
-  // for (i = 0; i < n;i++) {
-  //   printf("\nNhap ten sinh vien %d: ", i + 1);
-  //   gets(sinhVien[i]);
-  //   fflush(stdin);
-  // }
-  // 3
-  SortingString(sinhVien, n, com);
-  for (i = 0; i < n;i++) {
-    strcpy(sinhVien[i], format(StripSpace(sinhVien[i])));
-    printf("%d: %s\n", i + 1, sinhVien[i]);
-  }
-  // 4
-
 }
+void nhapN(int* n) {
+  printf("\nNhap so Nguyen: ");
+  scanf("%d", n); fflush(stdin);
+}
+void swap(char* A, char* B) {
+  char A_[strlen(A)]; strcpy(A_, A); strcpy(A, B); strcpy(B, A_);
+}
+char** SortingString(char** A, int len, int (*compare)(char*, char*)) {
+  int i; for (i = 0; i < len;i++) for (int j = 0; j < len - i;j++) if (compare(A[j], A[j + 1]) == 1) swap(A[j], A[j + 1]);
+}
+
 char* StripSpace(char* A) {
-  int len = strlen(A), lenC = 0;;
+  int len = strlen(A), lenC = 0, i, j;
   char* B = malloc((len + 1) * sizeof(char));
   strcpy(B, A);
-  if (isspace(B[len - 1])) for (int i = len - 1;i >= 0;i--) if (!isspace(B[i - 1])) {
+  if (isspace(B[len - 1])) for (i = len - 1;i >= 0;i--) if (!isspace(B[i - 1])) {
     B[i] = '\0';
     break;
   }
   char* C = malloc((strlen(B) + 1) * sizeof(char));
-  for (int i = 0, j = 0; i < strlen(B);i++) {
+  for (i = 0, j = 0; i < strlen(B);i++) {
     if (isspace(B[i]) && j == 0) continue;
-    C[j++] = B[i];
-    lenC = j;
+    C[j++] = B[i]; lenC = j;
   }
   C[lenC] = '\0';
   return C;
 }
 char* format(char* A) {
-  int len = strlen(A);
-  int lenC = len;
+  int len = strlen(A), lenC = len, i, c, j;
   char* C = malloc((len + 1) * sizeof(char));
-  for (int i = 0, c = 0, j = 0; i < len;i++) {
+  for (i = 0, c = 0, j = 0; i < len;i++) {
     if (isspace(A[i])) {
       if (c == 1) {
         lenC--;
